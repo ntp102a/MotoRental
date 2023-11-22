55b5ac1c858826caa76d492b490277c973dc78b1
ntp102a <ntp1022k2@gmail.com>
2023-11-22 17:40:06 +0700

    fix bug

--- MotoRental/Areas/Admin/Controllers/SearchController.cs
+++ MotoRental/Areas/Admin/Controllers/SearchController.cs
@@ -1,5 +1,4 @@
-﻿using MarketWeb.Models;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MotoRental.Models;
