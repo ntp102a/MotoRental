4a98acab8bbca508b2d30b38398a9f9ede8e2091
ntp102a <ntp1022k2@gmail.com>
2023-12-06 16:43:12 +0700

    Checkout

---
+++ MotoRental/Controllers/DonHangController.cs
@@ -0,0 +1,65 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using MotoRental.Models;
+using MotoRental.ModelViews;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace MotoRental.Controllers
+{
+    public class DonHangController : Controller
+    {
+        private readonly Rental_motorbikeContext _context;
+        public INotyfService _notyfService { get; }
+        public DonHangController(Rental_motorbikeContext context, INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+
+        //GET: DonHang/Details/5
+        [HttpPost]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var taikhoanID = HttpContext.Session.GetString("UserId");
+                if (string.IsNullOrEmpty(taikhoanID)) return RedirectToAction("Login", "Accounts");
+                var khachhang = _context.Users
+                    .AsNoTracking()
+                    .SingleOrDefault(x => x.UserId == Convert.ToInt32(taikhoanID));
+
+                if (khachhang == null) return NotFound();
+                var donhang= await _context.Rentals
+                    .Include(x => x.Status.StatusName)
+                    .FirstOrDefaultAsync(x => x.RentalId== id && Convert.ToInt32(taikhoanID) == x.UserId);
+                if (donhang == null) return NotFound();
+
+                var chitietdonhang = _context.RentalDetails
+                    .AsNoTracking()
+                    .Include(x => x.Vehicle)
+                    .Where(x => x.RentalId == id)
+                    .OrderBy(x => x.RentalDetailId)
+                    .ToList();
+                XemDonHang donHang = new XemDonHang();
+                donHang.DonHang = donhang;
+                donHang.ChiTietDonHang = chitietdonhang;
+                return PartialView("Details", donHang);
+            }
+            catch
+            {
+                return NotFound();
+            }
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MotoRental/ModelViews/MuaHangVM.cs
@@ -0,0 +1,25 @@
+﻿using MotoRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+
+namespace MotoRental.ModelViews
+{
+    public class MuaHangVM
+    {
+        public int UserId { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng nhập Họ và Tên")]
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng nhập Số điện thoại")]
+        public int Phone { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Địa chỉ nhận hàng")]
+        public string Address { get; set; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Vui lòng chọn Tỉnh/Thành")]
+        public string Note { get; set; }
+    }
+}
